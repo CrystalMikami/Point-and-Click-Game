49e0508b409728bc5a22dc70cf6e701f6678de57
CrystalMikami <clairehuang1681@outlook.com>
2022-01-03 14:57:35 -0800

    Added comments to Deck class

--- src/Model/Deck.java
+++ src/Model/Deck.java
@@ -3,9 +3,11 @@ package Model;
 import java.util.ArrayList;
 import java.util.Random;
 
+// Represents the full deck of cards available
 public class Deck {
     private ArrayList<Card> cardList = new ArrayList<>();
 
+    // Initializes a full deck
     public Deck() {
         for(int i = 1; i <= 13; i++) {
             for(int j = 1; j <= 4; j++) {
@@ -15,12 +17,15 @@ public class Deck {
         }
     }
 
+    // Draws a card from the available deck, the chosen card
+    // is removed from the deck
     public Card drawCard() {
         Random rand = new Random();
         int card = rand.nextInt(cardList.size());
         return cardList.remove(card);
     }
 
+    // Refreshes the deck
     public void refreshDeck() {
         cardList.clear();
         for(int i = 1; i <= 13; i++) {
