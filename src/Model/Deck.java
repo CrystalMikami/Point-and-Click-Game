bd5994df196b3459bece4254812c883e659ba607
CrystalMikami <clairehuang1681@outlook.com>
2022-01-03 14:32:44 -0800

    Deck class started

--- src/Model/Deck.java
+++ src/Model/Deck.java
@@ -1,4 +1,33 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Deck {
+    private ArrayList<Card> cardList;
+
+    public Deck() {
+        for(int i = 1; i <= 13; i++) {
+            for(int j = 1; j <= 4; j++) {
+                Card currCard = new Card(i, j);
+                cardList.add(currCard);
+            }
+        }
+    }
+
+    public Card drawCard() {
+        Random rand = new Random();
+        int card = rand.nextInt(cardList.size());
+        return cardList.remove(card);
+    }
+
+    public void refreshDeck() {
+        cardList.clear();
+        for(int i = 1; i <= 13; i++) {
+            for(int j = 1; j <= 4; j++) {
+                Card currCard = new Card(i, j);
+                cardList.add(currCard);
+            }
+        }
+    }
 }
