2f07b48356d3dca0a8e797a622002aaf51cbd673
CrystalMikami <clairehuang1681@outlook.com>
2022-01-03 15:36:40 -0800

    Added Board and Card methods

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -1,4 +1,43 @@
 package Model;
 
+// Represents the board
 public class Board {
+    public int difficulty; // 1 = easy, 2 = medium, 3 = hard
+    private Card[][] board;
+    private Deck deck;
+
+    // Initializes board size
+    public Board(int difficulty) {
+        deck = new Deck();
+        if (difficulty == 1) {
+            board = new Card[2][4]; // 8 Cards
+        } else if (difficulty == 2) {
+            board = new Card[4][8]; // 32 Cards
+        } else {
+            board = new Card[6][12]; // 72 Cards
+        }
+    }
+
+    // Add cards to the board
+    public void addCards() {
+        // Rows of board
+        for (int i = 0; i < board.length; i++) {
+            // Columns of board
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = deck.drawCard();
+            }
+        }
+    }
+
+    // Resets boards for new game
+    public void refreshBoard(int difficulty) {
+        deck.refreshDeck();
+        if (difficulty == 1) {
+            board = new Card[2][4]; // 8 Cards
+        } else if (difficulty == 2) {
+            board = new Card[4][8]; // 32 Cards
+        } else {
+            board = new Card[6][12]; // 72 Cards
+        }
+    }
 }
--- src/Model/Card.java
+++ src/Model/Card.java
@@ -6,12 +6,20 @@ public class Card {
     private int value; // Range from 1 to 13
     private int suit; // Range from 1 to 4; 1 is club, 2 is diamond
                       //                  ; 3 is heart, 4 is spades
+    private int flipped; // flipped = 1
 
     // MODIFIES: this
     // EFFECTS: sets up a card that has a value and a suit
     public Card (int value, int suit) {
         this.value = value;
         this.suit = suit;
+        this.flipped = 0;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the flipped state to flippedState
+    public void setFlipped(int flippedState) {
+        this.flipped = flippedState;
     }
 
     // EFFECTS: returns a card's value
@@ -22,5 +30,10 @@ public class Card {
     // EFFECTS: returns a card's suit
     public int getSuit() {
         return suit;
+    }
+
+    // EFFECTS: Returns if the card is flipped or not
+    public int getFlipped() {
+        return flipped;
     }
 }
