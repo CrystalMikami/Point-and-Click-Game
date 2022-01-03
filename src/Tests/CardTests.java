668ddcb2e8ff79fb7ce73006106473085af6ae89
olliepression <ollie96@students.cs.ubc.ca>
2022-01-03 14:18:10 -0800

    Completed Card class
    A card has a value and a suit.

--- src/Tests/CardTests.java
+++ src/Tests/CardTests.java
@@ -1,4 +1,31 @@
 package Tests;
 
+import Model.Card;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CardTests {
+
+    private Card testCardOne;
+    private Card testCardTwo;
+
+    @BeforeEach
+    public void setup() {
+        testCardOne = new Card(1 , 1);
+        testCardTwo = new Card(3, 4);
+    }
+
+    @Test
+    public void testGetValue() {
+        assertEquals(1, testCardOne.getValue());
+        assertEquals(3, testCardTwo.getValue());
+    }
+
+    @Test
+    public void testGetSuit() {
+        assertEquals(1, testCardOne.getSuit());
+        assertEquals(4, testCardTwo.getSuit());
+    }
 }
