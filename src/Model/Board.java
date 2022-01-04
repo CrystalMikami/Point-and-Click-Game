e888577d689507c5c55cf5fed29d59c96673fe05
olliepression <ollie96@students.cs.ubc.ca>
2022-01-03 21:18:23 -0800

    Completed tests in DeckTest and BoardTest.
    Set difficulty for board in constructor.
    Added row and column counts.
    Updated methods: Board.refreshBoard, Board.addCards
    New methods: Board.getCardAtPos
    New Field: Board.drawnCards

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -1,43 +1,79 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 // Represents the board
 public class Board {
     public int difficulty; // 1 = easy, 2 = medium, 3 = hard
     private Card[][] board;
+    private ArrayList<Card> drawnCards = new ArrayList<>();
     private Deck deck;
+    public int countRow = 0;
+    public int countColumn = 0;
 
     // Initializes board size
     public Board(int difficulty) {
         deck = new Deck();
         if (difficulty == 1) {
             board = new Card[2][4]; // 8 Cards
+            this.difficulty = 1;
+            countRow = 2;
+            countColumn = 4;
         } else if (difficulty == 2) {
             board = new Card[4][8]; // 32 Cards
+            this.difficulty = 2;
+            countRow = 4;
+            countColumn = 8;
         } else {
             board = new Card[6][12]; // 72 Cards
+            this.difficulty = 3;
+            countRow = 6;
+            countColumn = 12;
         }
     }
 
     // Add cards to the board
     public void addCards() {
+        for (int i = 0; i < (board.length * board[0].length) / 2; i++) {
+            Card current = deck.drawCard();
+            drawnCards.add(current);
+            drawnCards.add(current);
+        }
+
         // Rows of board
         for (int i = 0; i < board.length; i++) {
             // Columns of board
             for (int j = 0; j < board[0].length; j++) {
-                board[i][j] = deck.drawCard();
+                Random rand = new Random();
+                int drawnCardPos = rand.nextInt(drawnCards.size());
+                board[i][j] = drawnCards.remove(drawnCardPos);
             }
         }
     }
 
-    // Resets boards for new game
+    // Resets board for new game
     public void refreshBoard(int difficulty) {
         deck.refreshDeck();
         if (difficulty == 1) {
             board = new Card[2][4]; // 8 Cards
+            this.difficulty = 1;
+            countRow = 2;
+            countColumn = 4;
         } else if (difficulty == 2) {
             board = new Card[4][8]; // 32 Cards
+            this.difficulty = 2;
+            countRow = 4;
+            countColumn = 8;
         } else {
             board = new Card[6][12]; // 72 Cards
+            this.difficulty = 3;
+            countRow = 6;
+            countColumn = 12;
         }
+    }
+
+    public Card getCardAtPos(int row, int column) {
+        return board[row][column];
     }
 }
--- src/Tests/BoardTest.java
+++ src/Tests/BoardTest.java
@@ -1,5 +1,65 @@
 package Tests;
 
+import Model.Board;
+import Model.Card;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BoardTest {
 
+    Board testBoardEasy;
+    Board testBoardMedium;
+    Board testBoardHard;
+
+    @BeforeEach
+    public void setup() {
+        testBoardEasy = new Board(1);
+        testBoardMedium = new Board(2);
+        testBoardHard = new Board(3);
+    }
+
+    @Test
+    public void testAddCards() {
+        testBoardEasy.addCards();
+
+        for (int i = 0; i < testBoardEasy.countRow; i++) {
+            for (int j = 0; j < testBoardEasy.countColumn; j++) {
+                Card current = testBoardEasy.getCardAtPos(i, j);
+                assertFalse(current.equals(null));
+            }
+        }
+    }
+
+    @Test
+    public void testRefreshBoard() {
+        testBoardEasy.addCards();
+
+        testBoardEasy.refreshBoard(1);
+
+        assertEquals(2, testBoardEasy.countRow);
+        assertEquals(4, testBoardEasy.countColumn);
+
+        for (int i = 0; i < testBoardEasy.countRow; i++) {
+            for (int j = 0; j < testBoardEasy.countColumn; j++) {
+                Card current = testBoardEasy.getCardAtPos(i, j);
+                assertNull(current);
+            }
+        }
+
+        testBoardEasy.addCards();
+
+        testBoardEasy.refreshBoard(3);
+
+        assertEquals(6, testBoardEasy.countRow);
+        assertEquals(12, testBoardEasy.countColumn);
+
+        for (int i = 0; i < testBoardEasy.countRow; i++) {
+            for (int j = 0; j < testBoardEasy.countColumn; j++) {
+                Card current = testBoardEasy.getCardAtPos(i, j);
+                assertNull(current);
+            }
+        }
+    }
 }
